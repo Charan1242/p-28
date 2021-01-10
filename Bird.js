43f8c31f181e0322082fa09ee5caeafb9b6442ae
Charan1242 <74129657+Charan1242@users.noreply.github.com>
2021-01-10 11:00:28 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage=loadImage("sprites/smoke.png")
+    this.trag=[];
+
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+    var position=[this.body.position.x,this.body.position.y]
+    this.trag.push(position);}
+
+    for(var i=0;i<this.trag.length;i++){
+    image(this.smokeImage,this.trag[i][0],this.trag[i][1])
+
+    }
+  }
+}
